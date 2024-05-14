ff402200a8e2fcd9a0134708aac554ccc8522def
Vivek Saliya <viveksaliya004@gmail.com>
2024-05-14 21:31:50 +0530

    add: footer-links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,18 +15,31 @@ const Footer = () => {
       </p>
       <p className="footer-creator">
         <span>
-          Made with <i class="fa-solid fa-heart"></i> & ReactJS by Vivek Saliya
+          Made with <i class="fa-solid fa-heart"></i> & ReactJS by{" "}
+          <a href="https://www.linkedin.com/in/viveksaliya/">
+            Vivek Saliya
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
         </span>
       </p>
       <p className="footer-links">
-        <a href="https://www.linkedin.com/in/viveksaliya/" target="_blank">
+        <a href="https://www.facebook.com/SDJInternational/" target="_blank">
+          <i className="fa-brands fa-facebook"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/school/sdjic/?originalSubdomain=in"
+          target="_blank"
+        >
           <i className="fa-brands fa-linkedin"></i>
         </a>
-        <a href="https://www.instagram.com/vivek.saliya/" target="_blank">
+        <a
+          href="https://www.instagram.com/sdj_international_college/"
+          target="_blank"
+        >
           <i className="fa-brands fa-instagram"></i>
         </a>
-        <a href="https://twitter.com/vs_2611" target="_blank">
-          <i className="fa-brands fa-x-twitter"></i>
+        <a href="https://www.youtube.com/@sdjicevents5641" target="_blank">
+          <i className="fa-brands fa-youtube"></i>
         </a>
       </p>
     </div>
