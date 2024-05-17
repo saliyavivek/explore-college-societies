32e10c2341a79e3caa59c444b745e42ed9caf46a
Vivek Saliya <viveksaliya004@gmail.com>
2024-05-17 14:36:15 +0530

    fix: Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import sdj from "/images/sdj.jpg";
 
 const Banner = () => {
   return (
     <div className="banner-container">
+      <div className="banner-img">
+        <img src={sdj} alt="" />
+      </div>
       <div className="banner-text">
         <h1>Explore and Engage with Our College</h1>
         <div>
